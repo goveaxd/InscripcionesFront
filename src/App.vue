0ebc83a92fb7b186b6da825df6d3ab9184ae8e09
Govii <49994798+goveaxd@users.noreply.github.com>
2020-04-25 17:13:11 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Este es el proyecto que vamos a hacer algún dia no muy lejano jajaja"/>
+    <HelloWorld msg="Este es el proyecto que vamos a hacer algún dia no muy lejano jajaja, eso espero"/>
   </div>
 </template>
 

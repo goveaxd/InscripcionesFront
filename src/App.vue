36c4fa5db461e388f9a2f639531447364b307554
ceciliaCazares <ceci_ac@hotmail.com>
2020-05-12 21:51:55 -0500

    interfaces perronas de ceci jijij

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <template>
   <div id="app">
     <navbar></navbar>
-    <comprobacionPago></comprobacionPago>
-
-
+    <busquedaAlumnos></busquedaAlumnos>
     
   </div>
  
 </template>
 
 <script>
-
+import busquedaAlumnos from '@/components/busquedaAlumnos'
 import cDocentes from '@/components/consultarDocentes'
 import cMaterias from '@/components/consultarMateriasPCarrera'
 import prueba from '@/components/prueba'
-
+import sesion from '@/components/sesion'
 import horario from '@/components/horario'
 import consultaCarreras from '@/components/consultaCarreras'
 import kardex from '@/components/kardex.vue'
@@ -26,7 +24,12 @@ import comprobacionPago from '@/components/comprobacionPago'
 export default {
   name: 'App',
   components: {
-    cDocentes,cMaterias,kardex,prueba,horario, consultaCarreras,cargaAcademica,navbar, reinscripcionTerminada, comprobacionPago
+    cDocentes, cMaterias,
+    kardex, prueba, busquedaAlumnos,
+    horario, consultaCarreras,
+    cargaAcademica, navbar, 
+    reinscripcionTerminada, comprobacionPago,
+    sesion
   }
 }
 </script>

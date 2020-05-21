206d1a4cf1f042f7b295de8db15cc8444d4dc3e6
Govii <49994798+goveaxd@users.noreply.github.com>
2020-05-21 01:11:38 -0500

    Inserción de carga academica

--- views/cargaAcademica/board.vue
+++ views/cargaAcademica/board.vue
@@ -9,14 +9,27 @@
 </template>
 
 <script>
+let idM=(idMateria)=>{
+    let matAgreg=[]
+    try{
+        matAgreg=JSON.parse(localStorage.matAgreg)
+    }catch{}
+    let verif=matAgreg.indexOf(idMateria);
+    if(verif<0){
+        matAgreg.push(idMateria)
+    }else{
+         matAgreg.splice(verif,1)
+    }
+    localStorage.setItem("matAgreg",JSON.stringify(matAgreg));
+};
 export default {
     props: ['id'],
     methods:{
         drop: e=>{
             const card_id = e.dataTransfer.getData('card_id');
             const card = document.getElementById(card_id);
+            idM(card_id);
             e.target.appendChild(card);
-            alert(card_id); 
         }
     }
 }
